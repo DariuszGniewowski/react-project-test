3be8d1631e03b92188a7892b567e19f757967545
Dariusz Gniewowski <dariusz.gniewowski@gmail.com>
2025-02-01 15:02:38 +0100

    add side panel

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,6 +9,37 @@ const navLinks = [
     { title: "Shop", to: "/shop2", icon: "feather-shopping-bag" },
 ];
 
+const feedElements = [
+    { title: "Newsfeed", to: "/", icon: "feather-tv", color: "blue" },
+    { title: "Badges", to: "/defaultbadge", icon: "feather-award", color: "red" },
+    {
+        title: "Explore Stories",
+        to: "/defaultstorie",
+        icon: "feather-globe",
+        color: "gold",
+    },
+    {
+        title: "Popular Groups",
+        to: "/defaultgroup",
+        icon: "feather-zap",
+        color: "mini",
+    },
+    {
+        title: "Author Profile",
+        to: "/userpage",
+        icon: "feather-user",
+        color: "primary",
+    },
+];
+
+const morePages = [
+    { title: "Email Box", to: "/defaultemailbox", icon: "feather-inbox" },
+    { title: "Near Hotel", to: "/defaulthotel", icon: "feather-home" },
+    { title: "Latest Event", to: "/defaultevent", icon: "feather-map-pin" },
+    { title: "Live Stream", to: "/defaultlive", icon: "feather-youtube" }
+]
+
+
 export const Layout = ({ children }) => (
     <Fragment>
 
@@ -20,7 +51,6 @@ export const Layout = ({ children }) => (
                         Sociala.
                     </span>
                 </Link>
-
 
             </div>
             <form action="#" className="float-left header-search ms-3">
@@ -38,6 +68,7 @@ export const Layout = ({ children }) => (
                 <NavLink
                     key={navLink.title}
                     activeClassName="active"
+
                     to={navLink.to}
                     className="p-2 text-center ms-0 menu-icon center-menu-icon"
                 >
@@ -59,8 +90,59 @@ export const Layout = ({ children }) => (
             <Link to="defaultsettings" className="p-0 ms-3 menu-icon">
                 <img src="assets/images/user.png" alt="user" className="w40 mt--1" />
             </Link>
+            {/* Side Navigation */}
+            <nav className={`navigation scroll-bar`}>
+                <div className="container ps-0 pe-0">
+                    <div className="nav-content">
+                        <div className="nav-wrap bg-white bg-transparent-card rounded-xxl shadow-xss pt-3 pb-1 mb-2 mt-2">
+                            <div className="nav-caption fw-600 font-xssss text-grey-500">
+                                <span>New </span>Feeds
+                                <ul className="mb-1 top-content">
+                                    {feedElements.map((feedElement) => (
+                                        <li key={feedElement.to}>
+                                            <Link
+                                                to={feedElement.to}
+                                                className="nav-content-bttn open-font"
+                                            >
+                                                <i
+                                                    className={`${feedElement.icon} btn-round-md bg-${feedElement.color}-gradiant me-3`}
+                                                />
+                                                <span>{feedElement.title}</span>
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
 
-        </div>
+                        {/* More Pages */}
+                        <div className="nav-wrap bg-white bg-transparent-card rounded-xxl shadow-xss pt-3 pb-1 mb-2">
+                            <div className="nav-caption fw-600 font-xssss text-grey-500">
+                                <span>More </span>Pages
+                            </div>
+                            <ul className="mb-3">
+                                {morePages.map((morePage) => (
+                                    <li key={morePage.to}>
+                                        <Link
+                                            to={morePage.to}
+                                            className="nav-content-bttn open-font"
+                                        >
+                                            <i className={`font-xl text-current ${morePage.icon} me-3`} />
+                                            <span>{morePage.title}</span>
+                                            {morePage.count !== 0 && <span className="circle-count bg-warning mt-1">{morePage.count}</span>}
+                                        </Link>
+                                    </li>
+                                ))}
+
+                            </ul>
+                        </div>
+                        {/* More Pages */}
+                    </div>
+                </div>
+            </nav>
+            {/* /Side Navigation */}
+
+        </div >
         {children}
-    </Fragment>
+    </Fragment >
 );
