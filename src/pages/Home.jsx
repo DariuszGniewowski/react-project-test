9d050da0bccf46d0d713eb372d9ad3f848c1373f
Dariusz Gniewowski <dariusz.gniewowski@gmail.com>
2025-02-06 20:53:16 +0100

    uploader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Layout } from "../components/Layout/Layout";
 import { Postview } from "../components/PostView";
+import Loader from "../components/Loader";
 
 
 
@@ -17,7 +18,7 @@ export const Home = () => {
     console.log("posts from Home component:", posts);
 
 
-    const updatedPosts = posts.map((post) => {
+    const updatedPosts = posts?.map((post) => {
         return {
             ...post,
             postimage: `https://picsum.photos/id/${post.id}/800/400`,
@@ -31,9 +32,13 @@ export const Home = () => {
                     <div className="middle-sidebar-left">
                         <div className="row feed-body">
                             <div className="col-xl-8 col-xxl-9 col-lg-8">
-
-                                {updatedPosts && updatedPosts.length > 0 && updatedPosts.map((post) => (<Postview key={post.user} {...post} />))}
-
+                                {!updatedPosts || updatedPosts.length < 1 ? (
+                                    <Loader />
+                                ) : (
+                                    updatedPosts.map((post) => (
+                                        <Postview key={post.id} {...post} />
+                                    ))
+                                )}
                             </div>
                         </div>
                     </div>
